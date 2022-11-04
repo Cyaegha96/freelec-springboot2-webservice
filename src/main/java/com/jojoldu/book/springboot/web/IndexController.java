eaeeb056ecaff7ea01bb4df5e3640ac82c8ba03e
Cyaegha96 <zazae51@gmail.com>
2022-11-04 11:59:57 +0900

    세션 구글 로그인 기능

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.config.auth.dto.SessionUser;
 import com.jojoldu.book.springboot.service.posts.PostsService;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,16 +9,27 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         model.addAttribute("posts", postsService.findAllDesc());
+
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+            model.addAttribute("role",user.getRole());
+        }
 
         return "index";
     }
@@ -31,9 +43,13 @@ public class IndexController {
     public String postsUpdate(@PathVariable Long id, Model model){
 
         PostsResponseDto dto = postsService.findById(id);
+
         model.addAttribute("post", dto);
+
 
         return "posts-update";
     }
+
+
 
 }
