e1bd8394b3c757a06995a0ba74cb93affdc071c5
Cyaegha96 <zazae51@gmail.com>
2022-11-08 17:17:11 +0900

    Travis CI - AWS S3 - AWS Codedeploy 배포 deploy.sh 추가 (step2 환경에서 실행될 스크립트.) 및 travis.yml과 appespec.yml 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,6 @@
-#!bin/bash
+#!/bin/bash
 
 REPOSITORY=/home/ec2-user/app/step2
-PROJECT_NAME=freelec-springboot2-webservice
 
 echo ">Build 파일 복사"
 
@@ -25,7 +24,7 @@ echo ">새 애플리케이션 배포"
 
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
-echo ">JAR_NAME : $JAR_NAME"
+echo "> JAR Name: $JAR_NAME"
 
 echo ">$JAR_NAME 에 실행권한 추가"
 
@@ -33,7 +32,4 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup java jar \
-  -Dspring.config.location=classpath:/application.properties, classpath:/application-real.properties, /home/ec2-user/app/application-oauth.properties, /home/ec2-user/app/application-real-db.properties \
-  -Dspring.profiles.active=real \
-  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties,classpath:/application-real.properties -Dspring.profiles.active=real $REPOSITORY/$JAR_NAME 2>&1 &
