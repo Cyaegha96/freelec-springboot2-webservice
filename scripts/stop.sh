b82e90c5e3db9019d10edc672a435909ddd95c9d
Cyaegha96 <zazae51@gmail.com>
2022-11-10 17:05:30 +0900

    failed to deploy 원인 찾는 중

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -4,6 +4,8 @@ ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
+echo "> $ABSDIR 절대 경로"
+
 IDLE_PORT=$(find_idle_port)
 
 echo "> $IDLE_PORT 에서 구동중인 애플리케이션 pid 확인"
